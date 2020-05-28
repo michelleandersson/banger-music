725e869d8b9b7268274a520afc1a7f07b321cfdd
michelleandersson <michelleandersson@live.com>
2020-05-28 19:39:58 +0200

    get api working

--- src/script.js
+++ src/script.js
@@ -1,50 +1,19 @@
 const app = new Vue({
     el: '#vue-app', // document.getElementsByClassName('vue-app')[0]
     data: {
-      // name: 'Jane',
-      // name2: 'Alex',
-      // students: [
-      //   {
-      //     name: 'Jane',
-      //     age: 20
-      //   },
-      //   {
-      //     name: 'Bob',
-      //     age: 25
-      //   },
-      //   {
-      //     name: 'Alex',
-      //     age: 30
-      //   },
-      //   {
-      //     name: 'John',
-      //     age: 42
-      //   }
-      // ],
-      // names: ['Jane', 'Bob', 'Alex', 'John']
+      
       posts: []
     },
     methods: {
-      // showInfo: function(name) {
-      //   console.log(name);
-      // }
-      // getTime: function() {
-      //   return new Date();
-      // },
-      // changeName: function() {
-      //   this.name = 'Bob'
-      // },
-      // onInput(event) {
-      //   console.log(event.target.value);
-      //   this.name = event.target.value;
-      // }
+      
     },
     // Mounted hook is being called after the app is fully initialized
     mounted: function() {
-      fetch('http://api.musixmatch.com/ws/1.1/track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc')
+        fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?commontrack_id=5920049&apikey=e06708be7a728768734c486cd6c6547e')
         .then(response => response.json())
         .then(postsResponse => {
           this.posts = postsResponse;
         })
+        console.log("the app is loaded"); 
     }
   })
